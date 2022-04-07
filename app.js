bb7a0dce90c04d09a0f2e47fd2b3f967d6cc446e
Christianib003 <91727952+Christianib003@users.noreply.github.com>
2022-04-07 18:02:18 +0200

    Add files via upload
    Add javascript file

---
+++ app.js
@@ -0,0 +1,28 @@
+// MOBILE DROPDOWN MENU
+
+const menuBtn = document.querySelector('#bars-menu');
+const dropDown = document.querySelector('#menu-mobile');
+const closeBtn = document.querySelector('#close-btn');
+const body = document.getElementById('body');
+const options = document.querySelectorAll('.option');
+const overlay = document.querySelector('#background');
+
+menuBtn.addEventListener('click', () => {
+  dropDown.style.display = 'block';
+  overlay.style.display = 'block';
+  body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  dropDown.style.display = 'none';
+  overlay.style.display = 'none';
+  body.style.overflow = 'visible';
+});
+
+for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener('click', () => {
+    dropDown.style.display = 'none';
+    overlay.style.display = 'none';
+    body.style.overflow = 'visible';
+  });
+}
